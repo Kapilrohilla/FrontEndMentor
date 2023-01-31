475da3d595a468144c2fee965c4a1098eabce7d7
kapilRohilla <kapilrohilla2002@gmail.com>
2023-01-31 12:07:05 +0530

    Going live

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById('shareBtn');
+btn.addEventListener('click',()=>{
+    document.body.style.backgroundColor= "HotPink"
+})
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// add just for tailwind intelligence
+theme: {
+  extend: {
+    colors: {
+      clifford: '#da373d'
+    }
+  }
+}
