30f5140543b41639b5a8815e57a61aa0ae202ad4
kapilRohilla <kapilrohilla2002@gmail.com>
2023-02-18 21:29:38 +0530

    random advice generator completed

---
+++ random-advice-generator/script.js
@@ -0,0 +1,16 @@
+console.clear();
+function getData(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(r=>r.json())
+    .then(data=>{
+        const {id,advice} = data['slip'];
+        document.getElementById('advice').innerHTML = advice;
+        document.getElementById('id').innerText = `Advice #${id}`
+        console.log('data fetched');
+    })
+    .catch(()=>console.warn('Unable to fetch data'));
+};
+
+window.addEventListener('load',getData);
+
+document.getElementsByTagName('button')[0].addEventListener('click',getData)
